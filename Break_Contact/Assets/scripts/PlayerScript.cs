9a2d30d7b4ed01e76ebc88109d02c95698a792d6
Chad18Z <credeurchad@gmail.com>
2018-02-10 12:18:56 -0700

    Player character now rotates with mouse.

--- Break_Contact/Assets/scripts/PlayerScript.cs
+++ Break_Contact/Assets/scripts/PlayerScript.cs
@@ -6,13 +6,15 @@ public class PlayerScript : MonoBehaviour {
 
     Rigidbody2D rb; // rigidbody for collision
     float moveUnits = 5;
-    float speed = 2.0f; // speed of rotation
-    Vector3 startPosition;
+    float speed = 10.0f; // speed of rotation
+    Vector3 lastMousePosition;
+   
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent < Rigidbody2D>();
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,31 +24,48 @@ public class PlayerScript : MonoBehaviour {
     // Gets user input and moves the player character
     void FixedUpdate()
     {
-        float moveX = 0; 
-        float moveY = 0;
+        bool move = false;
+        Vector2 newPosition = Vector2.zero;
 
-        // check for horizontal input
-        if (Input.GetKey(KeyCode.A))
+        // this will rotate the character
+        if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
         {
-            moveX = -1;
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+            transform.up = direction;
         }
-        if (Input.GetKey(KeyCode.D))
+
+        // move player character to the right
+        if (Input.GetAxisRaw("Horizontal") > 0)
         {
-            moveX = 1;
+            newPosition = new Vector2(transform.position.x + .1f, transform.position.y);
+            move = true;
         }
-        // check for vertical input
-        if (Input.GetKey(KeyCode.W))
+        // move player character to the right
+        if (Input.GetAxisRaw("Horizontal") < 0)
         {
-            moveY = 1;
+            newPosition = new Vector2(transform.position.x - .1f, transform.position.y);
+            move = true;
         }
-        if (Input.GetKey(KeyCode.S))
+
+        // move player character to the right
+        if (Input.GetAxisRaw("Vertical") > 0)
         {
-            moveY = -1;
+            newPosition = new Vector2(transform.position.x , transform.position.y + .1f);
+            move = true;
         }
-        Vector2 position = rb.position;
-        position.x += moveX * moveUnits * Time.deltaTime;
-        position.y += moveY * moveUnits * Time.deltaTime;
-        rb.MovePosition(position);
+        // move player character to the right
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            newPosition = new Vector2(transform.position.x, transform.position.y - .1f);
+            move = true;
+        }
+        if (move)
+        {
+            rb.MovePosition(newPosition);
+            move = false;
+        }
+
 
     }
 }
