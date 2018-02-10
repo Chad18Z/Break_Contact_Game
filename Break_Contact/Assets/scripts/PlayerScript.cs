6b20f18e81860f73515f3022bd663f1451d8af93
Chad18Z <credeurchad@gmail.com>
2018-02-09 22:34:03 -0700

    Created player character with horizontal movement.

---
+++ Break_Contact/Assets/scripts/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    Rigidbody2D rb; // rigidbody for collision
+    float moveUnits = 10;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent < Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (horizontalInput != 0)
+        {
+            Vector2 position = rb.position;
+            position.x += horizontalInput * moveUnits * Time.deltaTime;
+            rb.MovePosition(position);
+        }
+    }
+}
